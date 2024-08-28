1723e82324d5f309b6c4ef36b6f754481ef0af2d
Konrad <56551801+aplik3030@users.noreply.github.com>
2024-08-28 09:59:26 +0200

    feat: Add initial backup script

---
+++ backup_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Variables
+BACKUP_SRC="/tmp"
+BACKUP_DST="/backups"
+BACKUP_DATE=$(date +%Y%m%d%H%M%S)
+BACKUP_FILENAME="backup_$BACKUP_DATE.tar.gz"
+
+mkdir -p "$BACKUP_DST/$BACKUP_DATE"
+
+# Archive the src dir
+tar -czf "$BACKUP_DST/$BACKUP_DATE_/$BACKUP_FILENAME" "$BACKUP_SRC"
+
+# Verify if backup succeded
+if [ $? -eq 0 ]; then
+	echo "Backup successful: $BACKUP_FILENAME"
+else
+	echo "Backup failed"
+fi
